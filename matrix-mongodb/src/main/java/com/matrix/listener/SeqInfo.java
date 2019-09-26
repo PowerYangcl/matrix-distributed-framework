f7f15474b952c061ee5d65091b859256e9c6c186
Yangcl <794867067@qq.com>
2019-09-26 15:17:49 +0800

    2

--- matrix-mongodb/src/main/java/com/matrix/listener/SeqInfo.java
+++ matrix-mongodb/src/main/java/com/matrix/listener/SeqInfo.java
@@ -1,7 +1,6 @@
 package com.matrix.listener;
 
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
