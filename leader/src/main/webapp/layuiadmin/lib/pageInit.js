4087ae990027f095c25839b23f1739deb8240dd8
Yangcl <794867067@qq.com>
2019-10-14 10:53:13 +0800

    2

--- leader/src/main/webapp/layuiadmin/lib/pageInit.js
+++ leader/src/main/webapp/layuiadmin/lib/pageInit.js
@@ -1,8 +1,12 @@
 
-
 /**
- * pageInit对象用于整个页面绘制和初始化的过程
- * 使用localStorage，当用户关闭浏览器或点击退出按钮则失效。
+ * @description: pageInit对象用于整个页面绘制和初始化的过程 | 使用localStorage，当用户关闭浏览器或点击退出按钮则失效。
+ * 	lib/admin.js使用此对象
+ * 
+ * @author Yangcl
+ * @home https://github.com/PowerYangcl
+ * @date 2019年10月14日 上午10:32:47 
+ * @version 1.0.0.1
  */
 layui.extend(
 {
@@ -120,30 +124,9 @@ layui.extend(
 			}
 			
 			layui.$("#LAY-system-side-menu").append(html_);
+			layui.$(".layui-nav-bar").attr("class", "zorder:999;");   // 动态生成左边菜单layui-nav-bar不起效果
 		},
 		
-		// 显示被隐藏的按钮| pageInit.security();   display:none;
-		security:function(){
-			if( typeof(localStorage.btns) != "undefined" && localStorage.btns.length != 0){
-				var barr = localStorage.btns.split(",");
-				for(var i = 0 ; i < barr.length ; i ++){
-					var key = barr[i].split("@")[1];
-					layui.$(".security-btn[key='" + key + "']").show();
-					layui.$(".security-btn[key='" + key + "']").removeClass("security-btn"); 
-				}
-				layui.$(".security-btn").remove();
-			}
-		},
-
-		/**
-		 * 二级菜单 单击事件
-		 * @param obj
-		 */
-		menuOnclick:function(obj){ 
-			var href_ = layui.$(obj).attr("target_"); 
-			localStorage.btns = layui.$(obj).attr("btns");  
-	    	layui.$("#sub-page").attr("src" , href_); 
-		}
 	}
 	
 	pageInit.drawPage = function(){
@@ -152,6 +135,30 @@ layui.extend(
 		pageInit.drawSystemPage();
 		layui.element.render(); // 动态生成菜单后 render 左侧下导航
 	},
+	
+	
+	// 显示被隐藏的按钮| pageInit.security();   display:none;
+	pageInit.security = function(){
+		if( typeof(localStorage.btns) != "undefined" && localStorage.btns.length != 0){
+			var barr = localStorage.btns.split(",");
+			for(var i = 0 ; i < barr.length ; i ++){
+				var key = barr[i].split("@")[1];
+				layui.$(".security-btn[key='" + key + "']").show();
+				layui.$(".security-btn[key='" + key + "']").removeClass("security-btn"); 
+			}
+			layui.$(".security-btn").remove();
+		}
+	},
+	
+	/**
+	 * 二级菜单 单击事件
+	 * @param obj
+	 */
+	pageInit.menuOnclick = function(obj){ 
+		var href_ = layui.$(obj).attr("target_"); 
+		localStorage.btns = layui.$(obj).attr("btns");  
+    	layui.$("#sub-page").attr("src" , href_); 
+	}
 	
 	//对外暴露的接口
 	exports('pageInit', pageInit);
