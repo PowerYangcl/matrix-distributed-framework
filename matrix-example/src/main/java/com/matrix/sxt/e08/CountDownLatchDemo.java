8172c530b19152db200bbd64d9d9671759eee9ea
PowerYangcl <power_matrix_mail@163.com>
2020-03-17 17:31:38 +0800

    2

--- matrix-example/src/main/java/com/matrix/sxt/e08/CountDownLatchDemo.java
+++ matrix-example/src/main/java/com/matrix/sxt/e08/CountDownLatchDemo.java
@@ -1,14 +1,15 @@
 package com.matrix.sxt.e08;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class CountDownLatchDemo {
-
     public static void main(String[] args) {
         CountDownLatch countDownLatch = new CountDownLatch(6);
-
         for (int i = 1; i <= 6 ; i ++){
+        	final int sleep = i;
             new Thread(() -> {
+            	try {TimeUnit.SECONDS.sleep(sleep);} catch (InterruptedException e) {e.printStackTrace();}
                 System.out.println(Thread.currentThread().getName() + "  国，被灭");
                 countDownLatch.countDown();
             }, CountryEnmu.foreach(i).getName() ).start();
@@ -18,5 +19,20 @@ public class CountDownLatchDemo {
             try { countDownLatch.await(); } catch (InterruptedException e) { e.printStackTrace(); }
             System.out.println(Thread.currentThread().getName() + "  一统华夏！");
         }, "秦国" ).start();
+        
+        new Thread(() -> {
+            try { countDownLatch.await(); } catch (InterruptedException e) { e.printStackTrace(); }
+            System.out.println(Thread.currentThread().getName() + "  再次一统华夏！");
+        }, "汉朝" ).start();
     }
 }
+
+
+
+
+
+
+
+
+
+
