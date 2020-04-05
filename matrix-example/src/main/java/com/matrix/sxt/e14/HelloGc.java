98122661a677b5ffb1f5ec31cb090a064dea9100
PowerYangcl <794867067@qq.com>
2020-04-05 21:42:56 +0800

    2

--- matrix-example/src/main/java/com/matrix/sxt/e14/HelloGc.java
+++ matrix-example/src/main/java/com/matrix/sxt/e14/HelloGc.java
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 public class HelloGc {
     public static void main(String[] args) {
-        byte[] array = new byte[50 * 1024 * 1024];  // 模拟一个数组大小为50M
+//        byte[] array = new byte[50 * 1024 * 1024];  // 模拟一个数组大小为50M
 
     }
 }
