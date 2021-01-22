0fa2c2d2af27bcabf2ca246907ccc77cb4869cfa
yangchenglin <yangchenglin11@jd.com>
2021-01-22 19:49:49 +0800

    1

---
+++ matrix-core/src/main/java/com/matrix/launch/RedisLaunch.java
@@ -0,0 +1,5 @@
+package com.matrix.launch;
+
+public class RedisLaunch {
+
+}
---
+++ matrix-core/src/main/java/com/matrix/launch/service/ContextLaunch.java
@@ -0,0 +1,19 @@
+package com.matrix.launch.service;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: Spring配置核心入口类
+ * 
+ * @author Yangcl
+ * @date 2021-1-21 18:45:33
+ * @home https://github.com/PowerYangcl
+ * @path matrix-core/com.matrix.launch.Launch.java
+ * @version 1.0.0.1
+ */
+@Configuration
+@ComponentScan({"com.matrix.dao,com.matrix.service,com.matrix.rpcimpl"})
+public class ContextLaunch {
+
+}
