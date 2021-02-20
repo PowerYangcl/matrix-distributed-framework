8c9460e372a114e7a486dba6a6d457601ddbf5ca
yangchenglin <yangchenglin11@jd.com>
2021-02-20 17:36:28 +0800

    2

--- matrix-core/src/main/java/com/matrix/launch/service/ContextLaunch.java
+++ matrix-core/src/main/java/com/matrix/launch/service/ContextLaunch.java
@@ -1,7 +1,10 @@
 package com.matrix.launch.service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.matrix.system.listener.MatrixDistributedFrameworkListener;
 
 /**
  * @description: Spring配置核心入口类
@@ -12,8 +15,13 @@ import org.springframework.context.annotation.Configuration;
  * @path matrix-core/com.matrix.launch.Launch.java
  * @version 1.0.0.1
  */
-//@Configuration
-//@ComponentScan({"com.matrix.dao,com.matrix.service,com.matrix.rpcimpl"})
+@Configuration
+@ComponentScan({"com.matrix.dao,com.matrix.service,com.matrix.rpcimpl"})
 public class ContextLaunch {
+	
+	@Bean(name = "mdf-listener")
+	public MatrixDistributedFrameworkListener initBbean() {
+		return new MatrixDistributedFrameworkListener();
+	}
 
 }
