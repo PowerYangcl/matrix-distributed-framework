cbe04dca6778f67c26ff1e100e05fb24e0449722
yangchenglin11 <yangchenglin11@jd.com>
2023-05-18 23:19:00 +0800

    1、由于引入spring-cloud-starter-bootstrap模块去加载nacos中的配置文件，此处会导致【首次】加载上下文环境时e.getApplicationContext().getParent() != null，所以会导致bean加载失败，故增加额外判断：&& SpringCtxUtil.getContext() != null 2、spring-boot版本升级至2.0系列的最高版本：2.7.10 3、Spring Cloud Alibaba Version|Spring Cloud Version|Spring Boot Version 各个版本依赖关系：https://github.com/alibaba/spring-cloud-alibaba/wiki/版本说明 4、web项目从nacos远程加载配置文件功能修改完成。

--- matrix-core/src/main/java/com/matrix/system/listener/MatrixDistributedFrameworkListener.java
+++ matrix-core/src/main/java/com/matrix/system/listener/MatrixDistributedFrameworkListener.java
@@ -31,12 +31,20 @@ public class MatrixDistributedFrameworkListener implements ApplicationListener<C
 	 * @date 22018年9月14日 下午12:41:19
 	 * @home https://github.com/PowerYangcl
 	 * @version 1.0.0.1
+	 * 
+	 * @description: 由于引入spring-cloud-starter-bootstrap模块去加载nacos中的配置文件，此处会导致【首次】加载上下文环境
+	 * 		时e.getApplicationContext().getParent() != null，所以会导致bean加载失败，故增加额外判断：&& SpringCtxUtil.getContext() != null
+	 * 
+	 * @author Yangcl
+	 * @date 2023-5-18 23:14:41
+	 * @version v1.6.2-scII-version
 	 */
 	public void onApplicationEvent(ContextRefreshedEvent e) {
-		if(e.getApplicationContext().getParent() != null) {
+		if(e.getApplicationContext().getParent() != null && SpringCtxUtil.getContext() != null) {
 			log.info("MatrixDistributedFrameworkListener出现二次调用 ! ! ! ! !");
 			return;
 		}
+		log.info("MatrixDistributedFrameworkListener 第一次调用，ApplicationContext 上下文环境加载完成。");
 		ApplicationContext context = e.getApplicationContext(); 
 		SpringCtxUtil.setApplicationContext(context);
 		systemInit.initClass();
